579f65dc374a58b3e66e563eb79226f172cbf75b
Michael LeGore <thelegore@gmail.com>
2017-12-12 10:57:14 -0500

    Wrap muxrpc service
    Wrapping the service so that the manifest method is preserved. Added
    option to forward methods or not, not forwarding methods will mean that
    only new methods are served. Clients will then connect direct to the
    input and only go through this pipe for methods that this pipe generates
    directly.

--- index.js
+++ index.js
@@ -2,20 +2,42 @@ var MultiServer = require('multiserver')
 var muxrpc = require('muxrpc')
 var pull = require('pull-stream')
 
-module.exports = function (inputRemote, inputChannels, outputChannels, cb) {
+module.exports = function (inputRemote, inputChannels, outputChannels, forward, cb) {
+  if (!cb && 'function' === typeof forward) {
+    cb = forward
+    forward = true
+  }
 
   var ms = MultiServer(inputChannels)
 
   // connect to the pipe input
   ms.client(inputRemote, function (err, stream) {
     var sbot = muxrpc((err, manifest, sbot) => {
-      var close
+      var close, started
+
+      // If we forward piped in methods, include them in the _manifest
+      // otherwise exclude them, so they're local methods only
+      if (forward)
+        sbot.manifest = syncToAsync(manifest)
+
       var api = {
         start () {
+          started = true
+
+          // Wrap api in another object to override manifest as a getter
+          // without changing api exposed to plugins
+          var serveApi = {
+            manifest (cb) {
+              cb(null, sbot.manifest)
+            }
+          }
+
+          serveApi.__proto__ = sbot
+
           // create an rpc server to output
           var ms = MultiServer(outputChannels)
           close = ms.server(function (stream) {
-            var pipe = muxrpc(null, syncToAsync(manifest)) (sbot)
+            var pipe = muxrpc(null, sbot.manifest) (serveApi)
             pull(stream, pipe.createStream(), stream)
           })
         },
@@ -24,10 +46,15 @@ module.exports = function (inputRemote, inputChannels, outputChannels, cb) {
             close()
             close = null
           }
+        },
+        use (plugin, overwrite) {
+          if (started)
+            throw new Error('pipe already started, cannot load plugins anymore')
+          sbot.use(plugin, overwrite)
         }
       }
 
-        cb(null, api)
+      cb(null, api)
     }) ()
 
     pull(stream, sbot.createStream(), stream)
