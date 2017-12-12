3075d5fd8aaadc425700843ff916bbeae8af08c5
Michael LeGore <thelegore@gmail.com>
2017-12-12 09:53:39 -0500

    Add tests for initial commit
    Mainline scenario of taking input muxrpc service, and forwarding it to
    another service.

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-import MultiServer from "multiserver"
-import plugify from './plugify'
+var MultiServer = require('multiserver')
+var muxrpc = require('muxrpc')
+var pull = require('pull-stream')
 
-export function (inputRemote, inputChannels, outputChannels, cb) {
+module.exports = function (inputRemote, inputChannels, outputChannels, cb) {
 
   var ms = MultiServer(inputChannels)
 
   // connect to the pipe input
   ms.client(inputRemote, function (err, stream) {
-    var sbot = muxrpc(() => {
-
-      var keepalive = setInterval(sbot.whoami, 1e3)
-      sbot.close = fork(sbot.close, function () {
-        clearInterval(keepalive)
-      })
-
-    var close
+    var sbot = muxrpc((err, manifest, sbot) => {
+      var close
       var api = {
         start () {
           // create an rpc server to output
           var ms = MultiServer(outputChannels)
           close = ms.server(function (stream) {
-            var pipe = muxrpc(null, sbot.manifest) (sbot)
+            var pipe = muxrpc(null, syncToAsync(manifest)) (sbot)
             pull(stream, pipe.createStream(), stream)
-          }
+          })
         },
         close () {
           if (close) {
@@ -32,8 +27,26 @@ export function (inputRemote, inputChannels, outputChannels, cb) {
         }
       }
 
-      cb(null, api)
-    })()
+        cb(null, api)
+    }) ()
 
     pull(stream, sbot.createStream(), stream)
+  })
+}
+
+// Function to recursively convert all sync manifest methods to async
+function syncToAsync (manifest) {
+  var copy = {}
+
+  Object.keys(manifest).forEach(function(key) {
+    if ('string' !== typeof manifest[key]) {
+      copy[key] = syncToAsync(manifest[key])
+    } else if(manifest[key] === 'sync') {
+      copy[key] = 'async'
+    } else {
+      copy[key] = manifest[key]
+    }
+  })
+
+  return copy
 }
---
+++ test.js
@@ -0,0 +1,74 @@
+var test = require('tape')
+var pull = require('pull-stream')
+var muxrpc = require('muxrpc')
+var MultiServer = require('multiserver')
+
+var afterparty = require('.')
+var manifest = {
+  test: 'sync',
+  manifest: 'sync',
+  source: 'source'
+}
+
+var values = [0, 5, 2, 3]
+var api = {
+  test (input) {
+    return 'test:' + input;
+  },
+  source () {
+    return pull.values(values)
+  },
+  manifest () {
+    return manifest
+  }
+}
+
+var mainApi = muxrpc(null, manifest) (api)
+
+var ws = require('multiserver/plugins/ws')({port: 2345})
+var wsOut = require('multiserver/plugins/ws')({port: 2346})
+var ms = MultiServer([ws])
+
+var close = ms.server(function (stream) {
+  pull(stream, mainApi.createStream(), stream)
+})
+
+test('pipe through', function (t) {
+  t.plan(3)
+
+  var check = function (apiAtOtherEnd) {
+    apiAtOtherEnd.test('value', (err, ret) => {
+      t.notOk(err)
+      t.equal(api.test('value'), ret)
+    })
+
+    pull(
+      apiAtOtherEnd.source(),
+      pull.collect(function (err, ary) {
+        t.deepEqual(values, ary)
+      }))
+  }
+
+  afterparty('ws://localhost:2345', [ws], [wsOut], (err, api) => {
+    api.start()
+    var wsOut = require('multiserver/plugins/ws')({port: 2346})
+    var ms = MultiServer([ws])
+
+    ms.client('ws://localhost:2346', function (err, stream) {
+      var mux = muxrpc((err, manifest, apiAtOtherEnd) => {
+        apiAtOtherEnd._manifest = manifest
+        apiAtOtherEnd.manifest = function () {
+          return manifest
+        }
+
+        check(apiAtOtherEnd)
+      }) ()
+
+      pull(stream, mux.createStream(), stream)
+    })
+
+    t.on('end', api.close)
+  })
+
+  t.on('end', close)
+})
